92398ba53c32d5765d199ff473597bc23e69b6fb
Aditya Khare <khareaditya425@gmail.com>
2021-10-25 11:36:32 +0530

    first commi

---
+++ my_file.py
@@ -0,0 +1,20 @@
+import unittest
+  
+class TestStringMethods(unittest.TestCase):
+      
+    def setUp(self):
+        pass
+  
+    # Returns True if the string contains 4 a.
+    def test_strings_a(self):
+        self.assertEqual( 'a'*4, 'aaaa')
+  
+    # Returns True if the string is in upper case.
+    def test_upper(self):        
+        self.assertEqual('foo'.upper(), 'FOO')
+
+
+
+ 
+if __name__ == '__main__':
+    unittest.main()
